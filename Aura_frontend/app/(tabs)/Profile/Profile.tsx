05c4ae8628536d30619482d51d14fdc5d246a39a
watagalawgbn <watagalawgbn.21@uom.lk>
2025-08-04 12:36:33 +0530

    profile screen's styles added to seperate file

--- Aura_frontend/app/(tabs)/Profile/Profile.tsx
+++ Aura_frontend/app/(tabs)/Profile/Profile.tsx
@@ -2,12 +2,12 @@ import React, { useState } from "react";
 import {
   View,
   Text,
-  StyleSheet,
   TextInput,
   TouchableOpacity,
   StatusBar,
 } from "react-native";
-import { useAuth } from "../../context/AuthContext";
+import styles from "./Profile.styles";
+import { useAuth } from "../../../context/AuthContext";
 import { Ionicons } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
 
@@ -120,78 +120,3 @@ export default function Profile() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-    backgroundColor: "#fff",
-  },
-  back: {
-    width: 40,
-    height: 40,
-    borderRadius: 20,
-    backgroundColor: "white",
-    borderWidth: 1,
-    borderColor: "#52AE77",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  title: {
-    fontSize: 28,
-    fontWeight: "bold",
-    marginBottom: 20,
-  },
-  avatar: {
-    alignSelf: "center",
-    backgroundColor: "#E6F2EB",
-    width: 120,
-    height: 120,
-    borderRadius: 60,
-    justifyContent: "center",
-    alignItems: "center",
-    marginBottom: 30,
-  },
-  inputWrapper: {
-    position: "relative",
-    marginBottom: 12,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: "#80C29B",
-    borderRadius: 8,
-    padding: 10,
-    marginBottom: 16,
-    paddingRight: 40,
-  },
-  eyeIcon: {
-    position: "absolute",
-    right: 10,
-    top: 12,
-  },
-  button: {
-    backgroundColor: "#80C29B",
-    paddingVertical: 12,
-    borderRadius: 10,
-    alignItems: "center",
-    marginTop: 10,
-  },
-  buttonText: {
-    color: "#fff",
-    fontWeight: "bold",
-    fontSize: 16,
-  },
-  logoutButton: {
-    backgroundColor: "#FF5C5C",
-    paddingVertical: 12,
-    paddingHorizontal: 24,
-    borderRadius: 10,
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 20,
-  },
-  logoutText: {
-    color: "#fff",
-    fontSize: 16,
-    fontWeight: "600",
-  },
-});
