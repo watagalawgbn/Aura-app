f406c4d47e760d61faa02e9982f19a6271a01ba9
watagalawgbn <watagalawgbn.21@uom.lk>
2025-08-19 11:00:09 +0530

    called the apiClient from  the mood service

--- Aura_frontend/app/services/moodService.ts
+++ Aura_frontend/app/services/moodService.ts
@@ -1,28 +1,17 @@
-//services.moodService.tsx
+//services.moodService.ts
 
-import axios from "axios";
-import { BASE_URL } from "@/constants/Api";
-import * as SecureStore from "expo-secure-store";
+import apiClient from "./apiClient";
 
 export const logMood = async(mood: string): Promise<void> => {
-
-    // retrieving token
-    const token = await SecureStore.getItemAsync("authToken");
-
-    // handle the error if token not found
-    if(!token){ 
-        throw new Error("Authentication token not found!");
-    }
-
     // send post request to backend to add mood
     try{ 
-        await axios.post(`${BASE_URL}/api/moods`,           //API end point
-            { mood },                                      //selected mood in the body
-            { headers: { Authorization: `Bearer ${token}`}} // auth header
+        await apiClient.post(
+            `/api/moods`, //API end point
+            { mood } //selected mood in the body
         );
     } 
     //throw any errors occured if request failed
-    catch(error: any){ 
+    catch(error){ 
         console.error("Error posting mood: ", error);
         throw new Error("Failed to log mood!");
     }
